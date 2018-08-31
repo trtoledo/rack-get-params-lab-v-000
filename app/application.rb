1d2686ba10da720fc76636ccc2634f52ade5fe6c
Tomas Toledo <trtoledo1977@gmail.com>
2018-08-31 04:03:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,6 +23,14 @@ class Application
           resp.write "#{item}\n"
         end
       end
+    elsif req.path.match(/add/)
+    search_term = req.params["item"]
+    if @@items.include?(search_term)
+      @@cart << search_term
+      resp.write "added #{search_term}"
+    else
+      resp.write "We don't have that item"
+    end
 
     else
       resp.write "Path Not Found"
